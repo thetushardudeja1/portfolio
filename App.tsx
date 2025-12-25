0d7ac373cb7bf2846d5b0ab17a78fcc043ca911a
thetushardudeja1 <tushar.dudeja09@gmail.com>
2025-12-25 10:59:09 +0530

    refactor: Remove unused IntroScene and scroll restoration logic
    The cinematic intro scene and manual scroll restoration logic were experimental and are no longer required. This commit removes them to simplify the App component and improve performance.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -7,35 +7,20 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import ScrollToTop from './components/ScrollToTop';
-import IntroScene from './components/IntroScene';
 
 const App: React.FC = () => {
-  useEffect(() => {
-    // We want natural scroll restoration for the "scrollytelling" effect to work if user refreshes mid-way
-    if ('scrollRestoration' in window.history) {
-      window.history.scrollRestoration = 'manual';
-    }
-    window.scrollTo(0, 0);
-  }, []);
-
   return (
     <div className="bg-background text-white min-h-screen selection:bg-blue-500/30 selection:text-blue-100">
-      {/* Scroll-driven Cinematic Intro */}
-      <IntroScene />
-
-      {/* Main Website Content */}
-      <div className="relative z-10 bg-background">
-        <Navbar />
-        <main>
-          <Hero />
-          <About />
-          <Focus />
-          <Experience />
-          <Projects />
-          <Contact />
-        </main>
-        <ScrollToTop />
-      </div>
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Focus />
+        <Experience />
+        <Projects />
+        <Contact />
+      </main>
+      <ScrollToTop />
     </div>
   );
 };
